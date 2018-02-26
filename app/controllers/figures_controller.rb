2cfb70287566a48752fe07bc8d8729c470cb1950
funemployeddad <funemployeddad@gmail.com>
2018-02-26 12:16:20 -0700

    got create new figure with existing or new title and/or landmark complete. working on diplaying one or all figures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,4 +4,22 @@ class FiguresController < ApplicationController
 
     erb :"/figures/new"
   end
+
+  post '/figures/new' do
+    @figure =  Figure.create(:name => params[:figure][:name])
+    if Title.find_by(:id => params[:figure][:title_ids])
+      @figure.titles << Title.find_by(:id => params[:figure][:title_ids])
+    else
+      @figure.titles << Title.create(:name => params[:title][:name])
+    end
+
+    if Landmark.find_by(:id => params[:figure][:landmark_ids])
+      @figure.landmarks << Landmark.find_by(:id => params[:figure][:landmark_ids])
+    else
+    @figure.landmarks << Landmark.create(:name => params[:landmark][:name])
+    end
+
+    redirect("/figures/#{@figure.slug}")
+  end
+
 end
--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,3 +1,10 @@
 class Landmark < ActiveRecord::Base
   belongs_to :figure
+  def slug
+  self.name.gsub(" ", "-").downcase
+end
+
+def self.find_by_slug(slug)
+  self.all.find{ |instance| instance.slug == slug}
+end
 end
